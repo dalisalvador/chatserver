9ed207ac489c4a2571fbba8fe10f39004213c130
F <f@MBP-de-F.lan>
2019-11-14 22:13:42 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route files
+const consorcios = require("./routes/consorcios");
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// // Mount routers
+app.use("/api/v1/consorcios", consorcios);
+
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Chat Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`${err.name}: ${err.message}`.red.bold);
+});
